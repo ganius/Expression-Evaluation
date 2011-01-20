bf22eeeb653d51907e544e0c8754faa16ca508ef
gnsmsk <gani1985@hotmail.com>
2011-01-20 18:16:29 +0200

    vs project and main files are added

---
+++ Main.py
@@ -0,0 +1,20 @@
+import sys
+import time
+import Operator
+import Tokenizer
+import Evaluator
+
+def main():
+    t = Tokenizer.Tokenizer()
+    e = Evaluator.Evaluator()
+    try:
+        exp = str(input("Enter expression: "))
+        rpn = t.shunting(exp)
+        result = e.evaluate(rpn)
+        print(result)
+    except IOError:
+        print("I/O error")
+
+if __name__ == '__main__':
+    main()
+    time.sleep(2)
